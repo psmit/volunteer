a04a4d14fb10e5741f132e693b85d3123da08630
Peter Smit <peter@smitmail.eu>
2012-06-30 22:01:04 +0300

    Add test case recognizer

--- volunteer/libs/__init__.py
+++ volunteer/libs/__init__.py
@@ -1,11 +1,12 @@
 from urllib import urlopen, urlencode
 
 
-def send_sms(number,message,settings,prefix="sms."):
+def send_sms(number,message,settings,prefix="sms.",from_name=None):
     base_url = "https://rest.nexmo.com/sms/json"
     api_key = settings[prefix+"key"]
     api_secret = settings[prefix+"secret"]
-    from_name = settings[prefix+"from"]
+    if from_name is None:
+        from_name = settings[prefix+"from"]
 
     response = urlopen("%s?%s"%(base_url,urlencode({'username':api_key,
                                                     'password':api_secret,
