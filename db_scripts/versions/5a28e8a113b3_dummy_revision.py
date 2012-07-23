4390e54dd2cf49708f850160c9a828d61875b1ac
Peter Smit <peter@smitmail.eu>
2012-07-23 17:35:40 +0300

    Created alembic environment

---
+++ db_scripts/versions/5a28e8a113b3_dummy_revision.py
@@ -0,0 +1,22 @@
+"""Dummy revision
+
+Revision ID: 5a28e8a113b3
+Revises: None
+Create Date: 2012-07-23 17:32:21.135163
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5a28e8a113b3'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
